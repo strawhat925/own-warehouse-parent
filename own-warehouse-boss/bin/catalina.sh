ab7f6fda14a8a60d88f02b4c6eb0555483bac32c
liz@yyft.com <543328410@qq.com>
2017-05-23 16:28:21 +0800

    maven plugin adjustment.

--- own-warehouse-boss/bin/catalina.sh
+++ own-warehouse-boss/bin/catalina.sh
@@ -12,27 +12,29 @@ cd ..
 case "$1" in
 
     start)
-        nohup java -Xms256m -Xmx512m -jar $JAR_NAME --server.port=8080 --spring.config.location=file:conf/db.properties >nohup.log 2>&1 &
+        java -Xms256m -Xmx512m -jar $JAR_NAME --server.port=8080 --spring.config.location=file:conf/db.properties >nohup.log 2>&1
+        #nohup java -Xms256m -Xmx512m -jar $JAR_NAME --server.port=8080 --spring.config.location=file:conf/db.properties >nohup.log 2>&1 &
+        ##exec java -Xms256m -Xmx512m -jar $JAR_NAME --server.port=8080 --spring.config.location=file:conf/db.properties &
         echo $! > $PID
-        echo "=== start $SERVICE_NAME"
+        echo "--------------- start $SERVICE_NAME"
         ;;
 
     stop)
         if [ -f "$PID" ]; then
             kill `cat $PID`
             rm -rf $PID
-            echo "=== stop $SERVICE_NAME"
+            echo "--------------- stop $SERVICE_NAME"
+        fi
 
-            sleep 5
-            ##
-            P_ID=`ps -ef | grep -w "$SERVICE_NAME" | grep -v "grep" | awk '{print $2}'`
-            if [ "$P_ID" == "" ]; then
-                echo "=== $SERVICE_NAME process not exists or stop success"
-            else
-                echo "=== $SERVICE_NAME process pid is:$P_ID"
-                echo "=== begin kill $SERVICE_NAME process, pid is:$P_ID"
-                kill -9 $P_ID
-            fi
+        sleep 5
+        ##
+        P_ID=`ps -ef | grep -w "$SERVICE_NAME" | grep -v "grep" | awk '{print $2}'`
+        if [ "$P_ID" == "" ]; then
+            echo "--------------- $SERVICE_NAME process not exists or stop success"
+        else
+            echo "--------------- $SERVICE_NAME process pid is:$P_ID"
+            echo "--------------- begin kill $SERVICE_NAME process, pid is:$P_ID"
+            kill -9 $P_ID
         fi
         ;;
 
@@ -40,7 +42,7 @@ case "$1" in
         $0 stop
         sleep 2
         $0 start
-        echo "=== restart $SERVICE_NAME"
+        echo "--------------- restart $SERVICE_NAME"
         ;;
 
     *)
@@ -51,4 +53,3 @@ case "$1" in
         ;;
 
 esac
-exit 0
